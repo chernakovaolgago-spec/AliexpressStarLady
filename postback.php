b2f41ccdb46887e8c6d2e9e1bf937e302cb6412c
chernakovaolgago-spec <chernakova.olga.go@gmail.com>
2025-09-13 21:15:05 +0300

    postback.php

--- postback.php
+++ postback.php
@@ -13,6 +13,14 @@ $payout  = $_GET['payout'] ?? '';
 $orderid = $_GET['orderid'] ?? '';
 $token   = $_GET['token'] ?? '';
 
+$country = $_GET['country'] ?? '';
+$platform = $_GET['platform'] ?? '';
+$item_id = $_GET['item_id'] ?? '';
+$new_buyer_bonus = $_GET['new_buyer_bonus'] ?? '';
+$is_not_product = $_GET['is_not_product'] ?? '';
+$base_commission_rate = $_GET['base_commission_rate'] ?? '';
+$order_platform = $_GET['order_platform'] ?? '';
+
 if ($token !== $secret_key) {
     file_put_contents($log_file, "Invalid token: $token\n", FILE_APPEND);
     http_response_code(403);
@@ -20,7 +28,5 @@ if ($token !== $secret_key) {
     exit;
 }
 
-$log_entry = "Valid request: ClickID=$clickid | Payout=$payout | OrderID=$orderid\n";
-file_put_contents($log_file, $log_entry, FILE_APPEND);
-
-echo "OK";
+$log_entry = "Valid request: ClickID=$clickid | Payout=$payout | OrderID=$orderid | Country=$country | Platform=$platform | ItemID=$item_id | Bonus=$new_buyer_bonus | IsNotProduct=$is_not_product | BaseRate=$base_commission_rate | OrderPlatform=$order_platform\n";
+file_put_contents($log_file, $log_entry, FILE_APPEND
